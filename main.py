802a62cc7981965c7244747338fae14383fb620d
chaochon <251132362@qq.com>
2024-11-08 00:50:14 +0800

    add compare method and metric

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from sklearn.datasets import fetch_california_housing
+from nltk import precision
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_squared_error, f1_score, accuracy_score, recall_score, confusion_matrix
+from sklearn.metrics import f1_score, accuracy_score, recall_score, confusion_matrix, precision_score
 from sklearn.model_selection import train_test_split
+import xgboost as xgb
 import numpy as np
 import pandas as pd
 
 
-class Predictor:
+class RandomForest:
     def __init__(self, n_estimators=100, max_depth=None, random_state=42):
         """初始化随机森林回归模型"""
         self.n_estimators = n_estimators
@@ -18,15 +23,13 @@ class Predictor:
             random_state=self.random_state
         )
 
-
-
-    def load_data(self):
-        """加载房价数据并拆分为训练集和测试集"""
-        data = fetch_california_housing()
-        X_train, X_test, y_train, y_test = train_test_split(
-            data.data, data.target, test_size=0.3, random_state=self.random_state
-        )
-        return X_train, X_test, y_train, y_test
+    # def load_data(self):
+    #     """加载房价数据并拆分为训练集和测试集"""
+    #     data = fetch_california_housing()
+    #     X_train, X_test, y_train, y_test = train_test_split(
+    #         data.data, data.target, test_size=0.3, random_state=self.random_state
+    #     )
+    #     return X_train, X_test, y_train, y_test
 
     def train(self, X_train, y_train):
         """训练模型"""
@@ -34,14 +37,151 @@ class Predictor:
 
     def evaluate(self, X_test, y_test):
         """评估模型性能"""
-        predictions = self.model.predict(X_test)
-        mse = mean_squared_error(y_test, predictions)
-        return mse
+        y_pred = self.model.predict(X_test)
+        # calculate metric
+        accuracy, recall, f1, precision, cm = calculate_metric(y_test, y_pred)
+        print("------------metric on the valid set for random forest-----------------------------")
+        print(f"Accuracy: {accuracy:.9f}")
+        print(f"Recall: {recall:.9f}")
+        print(f"F1-score: {f1:.9f}")
+        print(f"Precision (Macro Average): {precision * 100:.9f}%")
+        print('confusion matrix:\n', cm)
+        return (accuracy, recall, f1, precision)
+
 
     def test(self, X_test):
         predictions = self.model.predict(X_test)
         return predictions
 
+
+class DecisionTree:
+    def __init__(self, criterion='gini', max_depth=None, random_state=42):
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.random_state = random_state
+        self.model = DecisionTreeClassifier(
+            criterion=self.criterion,
+            max_depth=self.max_depth,
+            random_state=self.random_state
+        )
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def evaluate(self, X_test, y_test):
+        y_pred = self.model.predict(X_test)
+        # calculate metric
+        accuracy, recall, f1, precision, cm = calculate_metric(y_test, y_pred)
+        print("------------metric on the valid set for decision tress----------------------------")
+        print(f"Accuracy: {accuracy:.9f}")
+        print(f"Recall: {recall:.9f}")
+        print(f"F1-score: {f1:.9f}")
+        print(f"Precision (Macro Average): {precision * 100:.9f}%")
+        print('confusion matrix:\n', cm)
+        return (accuracy, recall, f1, precision)
+
+    def test(self, X_test):
+        predictions = self.model.predict(X_test)
+        return predictions
+
+class XGBoost:
+    def __init__(self, num_class=3, n_estimators=100, max_depth=None, random_state=42):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.random_state = random_state
+        self.num_class = num_class
+        self.model = xgb.XGBClassifier(
+            objective='multi:softmax',  # 多分类
+            num_class = num_class,  # 类别数
+            n_estimators=self.n_estimators,  # 树的数量
+            learning_rate=0.1,  # 学习率
+            max_depth=self.max_depth,  # 树的最大深度
+            random_state=self.random_state  # 随机种子
+        )
+
+    def train(self, X_train, y_train):
+        """训练模型"""
+        self.model.fit(X_train, y_train)
+
+    def evaluate(self, X_test, y_test):
+        """评估模型性能"""
+        y_pred = self.model.predict(X_test)
+        # calculate metric
+        accuracy, recall, f1, precision, cm = calculate_metric(y_test, y_pred)
+        print("------------these metric on the valid set for xgboost-----------------------------")
+        print(f"Accuracy: {accuracy:.9f}")
+        print(f"Recall: {recall:.9f}")
+        print(f"F1-score: {f1:.9f}")
+        print(f"Precision (Macro Average): {precision * 100:.9f}%")
+        print('confusion matrix:\n', cm)
+        return (accuracy, recall, f1, precision)
+
+    def test(self, X_test):
+        predictions = self.model.predict(X_test)
+        return predictions
+
+class SVM:
+    def __init__(self, kernel='rbf', C=1.0, gamma=0.1, coef0=0.0, random_state=42):
+        self.kernel = kernel
+        self.C = C
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.random_state = random_state
+        self.model = SVC(
+            kernel=self.kernel,
+            C=self.C,
+            gamma=self.gamma,
+            coef0=self.coef0,
+            random_state=self.random_state
+        )
+
+
+    def train(self, X_train, y_train):
+        """训练模型"""
+        self.model.fit(X_train, y_train)
+
+    def evaluate(self, X_test, y_test):
+        """评估模型"""
+        y_pred = self.model.predict(X_test)
+        # calculate metric
+        accuracy, recall, f1, precision, cm = calculate_metric(y_test, y_pred)
+        print("------------these metric on the valid set for svm-----------------------------")
+        print(f"Accuracy: {accuracy:.9f}")
+        print(f"Recall: {recall:.9f}")
+        print(f"F1-score: {f1:.9f}")
+        print(f"Precision (Macro Average): {precision * 100:.9f}%")
+        print('confusion matrix:\n', cm)
+        return (accuracy, recall, f1, precision)
+
+    def test(self, X_test):
+        predictions = self.model.predict(X_test)
+        return predictions
+
+class KNeighbors:
+    def __init__(self, n_neighbors = 5):
+        self.n_neighbors = n_neighbors
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+    def train(self, X_train, y_train):
+        """训练模型"""
+        self.model.fit(X_train, y_train)
+
+    def evaluate(self, X_test, y_test):
+        """评估模型"""
+        y_pred = self.model.predict(X_test)
+        # calculate metric
+        accuracy, recall, f1, precision, cm = calculate_metric(y_test, y_pred)
+        print("------------these metric on the valid set for k-meas -----------------------------")
+        print(f"Accuracy: {accuracy:.9f}")
+        print(f"Recall: {recall:.9f}")
+        print(f"F1-score: {f1:.9f}")
+        print(f"Precision (Macro Average): {precision * 100:.9f}%")
+        print('confusion matrix:\n', cm)
+        return (accuracy, recall, f1, precision)
+
+    def test(self, X_test):
+        predictions = self.model.predict(X_test)
+        return predictions
 
 # 定义一个函数来进行正弦和余弦编码
 def encode_season(season_column):
@@ -53,9 +193,19 @@ def encode_season(season_column):
     cos_values = season_column.apply(lambda x: np.cos(2 * np.pi * seasons.index(x) / period)).round(7)
     return sin_values, cos_values
 
+# 计算评价指标函数
+def calculate_metric(y_test, y_pred):
+    # 计算评价指标
+    y_pred, y_test = y_pred.astype(int), y_test.astype(int)
+
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    accuracy = accuracy_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred, average='weighted')
+    precision = precision_score(y_test, y_pred, average='weighted')
+    cm = confusion_matrix(y_test, y_pred)
+    return accuracy, recall, f1, precision, cm
 
 if __name__ == "__main__":
-
     # load raw data
     data_train_df = pd.read_csv('../dataset/train.csv')
     data_test_df = pd.read_csv('../dataset/test.csv')
@@ -92,19 +242,15 @@ if __name__ == "__main__":
             # 记录原季节特征名称
             season_col_names.append(season_col)
 
-    # 删除原来的季节列
+    # 4.删除原来的季节列
     data_train_df = data_train_df.drop(columns=season_col_names)
-
-    #-------------learn about data for debug------------
-    # write into file for read
-    # with open('output_train_clear2.txt', 'w') as f:
-    #     f.write(data_train_df.to_string(index=False))
 
     print("train sample length after convert season: {} features: {}".format(len(data_train_df), len(data_train_df.columns)))
 
     print("------------divide sample into train set and valid set------------")
     # 获取特征矩阵（从第二列到倒数第二列）
     X = data_train_df.iloc[:, 1:-1]  # 选择从第二列到倒数第二列
+    features_pd = X
 
     # 获取标签向量（最后一列）
     y = data_train_df.iloc[:, -1]  # 选择最后一列
@@ -113,31 +259,69 @@ if __name__ == "__main__":
     print("X_train len: {} y_train len: {} X_test len: {} y_test len: {}".format(len(X_train), len(y_train), len(X_test), len(y_test)))
     print("train label: \n", y_train.value_counts(), "\ntest label: \n", y_test.value_counts())
 
+    # all metric
+    metric_values = []
+    model_names = ['random forest', 'xgboost', 'decision tree', 'svm', 'k-meas']
+
     # random forest
-    predictor = Predictor(n_estimators=100, max_depth=10)
-    predictor.train(X_train, y_train)
-
-    # 计算指标
-    y_pred = predictor.test(X_test)
-    y_pred, y_test = y_pred.astype(int), y_test.astype(int)
-
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    accuracy = accuracy_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred, average='weighted')
-    cm = confusion_matrix(y_test, y_pred)
-
-    print("------------metric on the valid set-----------------------------")
-    print(f"Accuracy: {accuracy:.9f}")
-    print(f"Recall: {recall:.9f}")
-    print(f"F1-score: {f1:.9f}")
-    print('confusion matrix:\n', cm)
-
-    # decision tress
+    random_forest = RandomForest(n_estimators=100, max_depth=10)
+    # 训练模型
+    random_forest.train(X_train, y_train)
+    # 评估模型
+    metric_value = random_forest.evaluate(X_test, y_test)
+    metric_values.append(metric_value)
 
     # xgboost
+    xgboost = XGBoost(num_class=3, n_estimators=100, max_depth=3, random_state=42)
+    # 训练模型
+    xgboost.train(X_train, y_train)
+    # 评估模型
+    metric_value = xgboost.evaluate(X_test, y_test)
+    metric_values.append(metric_value)
+
+    # decision tress
+    decisiontress = DecisionTree(criterion='gini', max_depth=3, random_state=42)
+    # 训练模型
+    decisiontress.train(X_train, y_train)
+    # 评估模型
+    metric_value = decisiontress.evaluate(X_test, y_test)
+    metric_values.append(metric_value)
 
     # svm
+    svm = SVM(kernel='rbf', C=1.0, gamma=0.1, coef0=0.1, random_state=42)
+
+    # 数据标准化（SVM 对数据分布比较敏感，推荐进行标准化）
+    scaler = StandardScaler()
+    X_train_standard = scaler.fit_transform(X_train)
+    X_test_standard = scaler.transform(X_test)
+
+    # 训练模型
+    svm.train(X_train_standard, y_train)
+    # 评估模型
+    metric_value = svm.evaluate(X_test_standard, y_test)
+    metric_values.append(metric_value)
 
     # k-nearsetNeighb
+    kmeas = KNeighbors(n_neighbors=5)
+    # 训练模型
+    kmeas.train(X_train_standard, y_train)
+    # 评估模型
+    metric_value = kmeas.evaluate(X_test_standard, y_test)
+    metric_values.append(metric_value)
 
+    # make static table
+    metric_data = {'metric': ['accurary', 'recall', 'f1', 'precision']}
+    for i, model_name in enumerate(model_names):
+        metric_data[model_name] = metric_values[i]
+
+    # 创建DataFrame
+    metric_data_df = pd.DataFrame(metric_data)
+
+    # 导出为Excel文件
+    excel_path = 'model_evaluation_and_features.xlsx'
+    # 使用ExcelWriter对象写入两个工作表
+    with pd.ExcelWriter(excel_path, engine='xlsxwriter') as writer:
+        metric_data_df.to_excel(writer, sheet_name='Model Evaluation', index=False)
+        features_pd.to_excel(writer, sheet_name='Features', index=False)
+    print(f"Excel文件已生成并保存到 {excel_path}")
 
